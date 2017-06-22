e46059a19ae29a6e78bee150dba4663f50d59e38
Sean Livingston <sean.d.livingston@gmail.com>
2017-06-22 00:05:21 -0400

    Learning and adjusting to Objective-C

--- Objective-C-Tutorial-2/ViewController.m
+++ Objective-C-Tutorial-2/ViewController.m
@@ -10,13 +10,42 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) NSString *username;
+@property (nonatomic, strong) NSString *password;
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextfield;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextfield;
+@property (weak, nonatomic) IBOutlet UILabel *notificationLabel;
+
 @end
 
 @implementation ViewController
 
+- (IBAction)loginAction:(id)sender {
+    
+    BOOL isUserEqual = [self.username isEqualToString:[self.usernameTextfield text]];
+    BOOL isPasswordEqual = [self.password isEqualToString:[self.passwordTextfield text]];
+    
+    if (isUserEqual && isPasswordEqual) {
+        NSLog(@"Success");
+        [self.notificationLabel setText:@"Congratulations! You are logged in!"];
+    } else {
+        NSLog(@"Failure");
+        [self.notificationLabel setText:@"Login failed!"];
+    }
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.username = @"bob";
+    self.password = @"securePassword";
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.passwordTextfield.secureTextEntry = YES;
+    
+    
+    NSLog(@"%@ %@", self.username, self.password);
 }
 
 
@@ -25,5 +54,9 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [self.view endEditing:YES];
+}
 
 @end
