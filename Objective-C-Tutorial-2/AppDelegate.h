9d98750fea5de609e83bd1a1cfcf07654d2a021b
Sean Livingston <sean.d.livingston@gmail.com>
2017-06-21 23:26:08 -0400

    Initial Commit

---
+++ Objective-C-Tutorial-2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Objective-C-Tutorial-2
+//
+//  Created by Sean on 6/21/17.
+//  Copyright © 2017 Sean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Objective-C-Tutorial-2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Objective-C-Tutorial-2
+//
+//  Created by Sean on 6/21/17.
+//  Copyright © 2017 Sean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
